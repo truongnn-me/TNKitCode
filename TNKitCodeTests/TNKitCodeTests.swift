c85aac81a04dd9893fe91e10f42a924e22aacfa6
Truong Nguyen Nhut <nhuttruong88@gmail.com>
2019-02-26 16:29:45 +0700

    Initial Commit

---
+++ TNKitCodeTests/TNKitCodeTests.swift
@@ -0,0 +1,34 @@
+//
+//  TNKitCodeTests.swift
+//  TNKitCodeTests
+//
+//  Created by Truong Nguyen Nhut (Lazada Group) on 2/26/19.
+//  Copyright © 2019 Truong Nguyen Nhut. All rights reserved.
+//
+
+import XCTest
+@testable import TNKitCode
+
+class TNKitCodeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
