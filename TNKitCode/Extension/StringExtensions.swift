90c5b7ab0d6b6dbe461e540f39d96bcefda7dac6
Truong Nguyen Nhut <nhuttruong88@gmail.com>
2019-03-01 00:27:48 +0700

    add string Utils

--- TNKitCode/Extension/StringExtensions.swift
+++ TNKitCode/Extension/StringExtensions.swift
@@ -9,31 +9,110 @@
 import UIKit
 
 extension String {
-    
-    func width(withConstrainedHeight height: Float, font: UIFont) -> Float {
+    /**
+     Return the minimum width for display text in 1 line
+     */
+    func minimumWidthForDisplayInOneLine(withConstrainedHeight height: Float, font: UIFont) -> Float {
         let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: CGFloat(height))
         let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
         return ceilf(Float(boundingBox.width))
     }
     
+    /**
+     Find the space's index for break the text in 2 lines
+     Ex: let test = "abcde 7890". Return Index at 5
+     */
     func breakLineIndexForTwoLines() -> String.Index? {
-        var moveIndex = index(startIndex, offsetBy: count/2)
-        while moveIndex<endIndex && self[moveIndex] != " "  {
-            moveIndex = index(moveIndex, offsetBy: 1)
+        let moveIndex = index(startIndex, offsetBy: count/2)
+        if validIndex(index: moveIndex) {
+            if self[moveIndex] == " " {
+                return moveIndex
+            } else {
+                if let breaklineIndex1 = move(toString: " ", fromIndex: moveIndex, moveForward: true) {
+                    return breaklineIndex1
+                }
+                if let breaklineIndex2 = move(toString: " ", fromIndex: moveIndex, moveForward: false) {
+                    return breaklineIndex2
+                }
+            }
         }
-        if endIndex <= moveIndex {
-            return nil
-        }
-        return moveIndex
+        return nil
     }
     
+    /**
+     Minimum width for display texts in 2 lines
+     */
     func minimumWidthForTwoLines(withConstrainedHeight height: Float, font: UIFont) -> Float {
         if let breakLineIndex = self.breakLineIndexForTwoLines() {
             let string1 = String(self[startIndex..<breakLineIndex])
             let string2 = String(self[breakLineIndex..<endIndex])
-            return max(string1.width(withConstrainedHeight: height, font: font), string2.width(withConstrainedHeight: height, font: font))
+            return max(string1.minimumWidthForDisplayInOneLine(withConstrainedHeight: height, font: font),
+                       string2.minimumWidthForDisplayInOneLine(withConstrainedHeight: height, font: font))
         }
-        return self.width(withConstrainedHeight: height, font: font)
+        return self.minimumWidthForDisplayInOneLine(withConstrainedHeight: height, font: font)
+    }
+    
+    /**
+     Create a string with defined length and prefix
+     If cannot find the string with length defined, it will try to get the string which has length near this defined value.
+     - Parameters:
+        - length: length of returned string
+        - prefix: prefix for returned string. Its length is also counted in defined length
+     - Returns:
+        - The string returned will have defined length and prefix. Can be nil.
+     */
+    func string(length: Int, prefix: String) -> String? {
+        var result: String?
+        if count > 0 {
+            if count + prefix.count <= length {
+                result = prefix + self
+                return result
+            }
+            if let sliceIndex = index(startIndex, offsetBy: length - prefix.count, limitedBy: endIndex) {
+                if self[sliceIndex] != " " {
+                    //Move index until can reach the space
+                    if let moveIndex = move(toString: " ", fromIndex: sliceIndex, moveForward: false) {
+                        let stringSplit = String(self[startIndex..<moveIndex])
+                        
+                        result = prefix + stringSplit
+                    }
+                } else {
+                    let stringSplit = String(self[startIndex..<sliceIndex])
+                    result = prefix + stringSplit
+                }
+            } else {
+                let stringSplit = String(self[startIndex..<endIndex])
+                result = prefix + stringSplit
+            }
+        }
+        return result
+    }
+    
+    /**
+    Check if the index in range startIndex..<endIndex
+    */
+    func validIndex(index: String.Index) -> Bool {
+        return index >= startIndex && index < endIndex
+    }
+    
+    /**
+    Return toString's Index from fromIndex. The direction can be forward/backward
+    */
+    func move(toString string: String, fromIndex: String.Index, moveForward: Bool) -> String.Index? {
+        if fromIndex >= startIndex && fromIndex <= endIndex {
+            let offset = moveForward ? 1 : -1
+            let limitedIndex = moveForward ? endIndex : startIndex
+            var moveIndexStart = fromIndex
+            while let moveIndex = index(moveIndexStart, offsetBy: offset, limitedBy: limitedIndex) {
+                if moveIndex != limitedIndex {
+                    if String(self[moveIndex]) == string {
+                        return moveIndex
+                    }
+                }
+                moveIndexStart = moveIndex
+            }
+        }
+        return nil
     }
     
 }
--- TNKitCodeTests/New Group/TNStringTests.swift
+++ TNKitCodeTests/New Group/TNStringTests.swift
@@ -12,48 +12,147 @@ import XCTest
 @testable import TNKitCode
 
 class TNStringTests: XCTestCase {
-    func testStringCannotBreakline() {
+    func test_breakLineIndexForTwoLines_whenCannotFindIndexForBreakline_ShouldReturnNil() {
         let testString = "12345"
         let result = testString.breakLineIndexForTwoLines()
         XCTAssertNil(result)
     }
     
-    func testBreakline() {
-        let testString = "123 45"
+    func test_breakLineIndexForTwoLines_whenCanFindTheIndexForBreakline_shouldReturnTheIndex() {
+        var sub1 = "123"
+        var sub2 = " 45"
+        checkBreakLineIndexForTwoLines(sub1: sub1, sub2: sub2)
+        sub1 = "a"
+        sub2 = " bcdefghijk"
+        checkBreakLineIndexForTwoLines(sub1: sub1, sub2: sub2)
+        sub1 = "1234"
+        sub2 = " 45"
+        checkBreakLineIndexForTwoLines(sub1: sub1, sub2: sub2)
+    }
+    
+    func checkBreakLineIndexForTwoLines(sub1: String, sub2: String) -> Void {
+        let testString = sub1 + sub2
         if let result = testString.breakLineIndexForTwoLines() {
             let string1 = String(testString[testString.startIndex..<result])
             let string2 = String(testString[result..<testString.endIndex])
-            XCTAssert(string1 == "123", "")
-            XCTAssert(string2 == " 45", "")
+            XCTAssert(string1 == sub1, "")
+            XCTAssert(string2 == sub2, "")
         } else {
             XCTFail()
         }
     }
     
-    func testMidleIndexIsNonspaceCharacter() {
-        let testString = "1234 45"
-        if let result = testString.breakLineIndexForTwoLines() {
-            let string1 = String(testString[testString.startIndex..<result])
-            let string2 = String(testString[result..<testString.endIndex])
-            XCTAssert(string1 == "1234", "string1 should be 1234")
-            XCTAssert(string2 == " 45", "string2 should be ' 45'")
-        } else {
-            XCTFail()
-        }
-    }
-    
-    func testMidleIndexInEmptyString() {
+    func test_breakLineIndexForTwoLines_whenStringIsEmpty_shouldReturnNil() {
         let testString = ""
         let result = testString.breakLineIndexForTwoLines()
         XCTAssertNil(result)
     }
     
-    func testMinimumWidthForText() {
+    func test_minimumWidthForDisplayInOneLine() {
         let testString = "12345"
         let font = UIFont.systemFont(ofSize: 14)
         let expected = Float(41)//Get this number in UILabel from Xib file or storyboard
         let result = testString.minimumWidthForTwoLines(withConstrainedHeight: 34, font: font)
         XCTAssertEqual(expected, result)
-        
     }
+    
+    //MARK: test move method
+    func test_move_whenStringIsEmpty_shouldReturnNil() {
+        let testString = ""
+        let moveIndex1 = testString.move(toString: "a", fromIndex: testString.startIndex, moveForward: true)
+        XCTAssertNil(moveIndex1)
+        let moveIndex2 = testString.move(toString: "a", fromIndex: testString.startIndex, moveForward: false)
+        XCTAssertNil(moveIndex2)
+    }
+    
+    func test_move_whenCannotFindIndex_shouldReturnNil() {
+        let testString = "a bc bd df hg gg"
+        let moveIndex1 = testString.move(toString: "a", fromIndex: testString.startIndex, moveForward: true)
+        XCTAssertNil(moveIndex1)
+        
+        let moveIndex2 = testString.move(toString: "a", fromIndex: testString.startIndex, moveForward: false)
+        XCTAssertNil(moveIndex2)
+    }
+    
+    func test_move_whenCanFindIndex_shouldReturnIndexAtString() {
+        let testString = "abcs3fvfkjvbkfjfkg"
+        let findString = "g"
+        if let moveIndex = testString.move(toString: findString, fromIndex: testString.startIndex, moveForward: true) {
+            let string = String(testString[moveIndex])
+            XCTAssertEqual(string, findString)
+        } else {
+            XCTFail()
+        }
+        let moveIndex = testString.move(toString: findString, fromIndex: testString.startIndex, moveForward: false)
+        XCTAssertNil(moveIndex)
+    }
+    
+    func test_move_whenInvalidFromIndex_shouldReturnNil() {
+        let tempString = "1234567890"
+        let fromIndex = tempString.index(tempString.startIndex, offsetBy: 8)
+        let testString = "agbcs3"
+        let findString = "g"
+        let result1 = testString.move(toString: findString, fromIndex: fromIndex, moveForward: true)
+        let result2 = testString.move(toString: findString, fromIndex: fromIndex, moveForward: false)
+        XCTAssertNil(result1)
+        XCTAssertNil(result2)
+    }
+    //MARK: test string method
+    func test_string_withEmptyString_shouldReturnNil() {
+        let testString = ""
+        let result = testString.string(length: 10, prefix: "abc")
+        XCTAssertNil(result)
+    }
+    
+    func test_string_withHasLengthLessThanDefinedLength_shouldStringWithPrefixInLength() {
+        let testString = "a b c d e f g h"
+        let result = testString.string(length: 20, prefix: "abc")
+        let expected = "abca b c d e f g h"
+        XCTAssertEqual(result, expected)
+    }
+    
+    func test_string_whenSliceIndexIsSpace_shouldReturnStringWithPrefixInLength() {
+        let testString = "a bc bd df hg gg"
+        let prefix = "###"
+        let expected = prefix + "a bc bd"
+        let result = testString.string(length: 10, prefix: prefix)
+        XCTAssertTrue(result == expected)
+    }
+    
+    func test_string_whenSliceIndexIsNotSpace_shouldReturnStringWithPrefixNearestDefinedLength() {
+        let testString = "a bc bddf hg gg"
+        let prefix = "###"
+        let expected = prefix + "a bc"
+        let result = testString.string(length: 10, prefix: prefix)
+        XCTAssertTrue(result == expected)
+    }
+    
+    func test_string_withValidLengthAndPrefix_shouldReturnStringWithPrefixInLength() {
+        let testString = "a b c d s e w d f 1 23 4"
+        let result = testString.string(length: 5, prefix: "abc")
+        let expected = "abca"
+        XCTAssertEqual(result, expected)
+    }
+    
+    func test_string_withEmptyPrefixAndValidLength_shouldReturnStringWithEmptyPrefixAndLength() {
+        let testString = "a b c d s e w d f 1 23 4"
+        let result = testString.string(length: 5, prefix: "")
+        let expected = "a b c"
+        XCTAssertEqual(result, expected)
+    }
+    
+    func test_string_withDefinedLengthGreaterThanLengthOfString_shouldReturnNil() {
+        let testString = "a b c d s e w d f 1 23 4"
+        let prefix = "abc"
+        let result = testString.string(length: 50, prefix: prefix)
+        let expected = prefix + testString
+        XCTAssertEqual(expected, result)
+    }
+    
+    func test_string_withContainsWordExceedLength_shouldReturnNil() {
+        let testString = "abcdefgqwrrrnkvfnkvnfknfdnd"
+        let result = testString.string(length: 10, prefix: "ss")
+        XCTAssertNil(result)
+    }
+    
 }
