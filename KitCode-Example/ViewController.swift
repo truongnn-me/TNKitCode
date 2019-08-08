6663a86755693d7a2083381a41825ebfde86e40e
Truong Nguyen <truong@elsanow.io>
2019-08-08 17:52:01 +0700

    Edit gitignore file Add example target

---
+++ KitCode-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KitCode-Example
+//
+//  Created by Nguyen Nhut Truong on 8/8/19.
+//  Copyright © 2019 Truong Nguyen Nhut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KitCode-ExampleTests/KitCode_ExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  KitCode_ExampleTests.swift
+//  KitCode-ExampleTests
+//
+//  Created by Nguyen Nhut Truong on 8/8/19.
+//  Copyright © 2019 Truong Nguyen Nhut. All rights reserved.
+//
+
+import XCTest
+@testable import KitCode_Example
+
+class KitCode_ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
