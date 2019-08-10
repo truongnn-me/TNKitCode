116c3fd0e7a8f284ad5b68f869f93fc7ca0f43ed
Truong Nguyen <nhuttruong88@gmail.com>
2019-08-10 13:16:11 +0700

    Add space for random string

--- TNKitCode/Extension/StringExtensions.swift
+++ TNKitCode/Extension/StringExtensions.swift
@@ -205,7 +205,7 @@ public extension String {
     }
     
     static func randomString(inLength length: Int) -> String {
-        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let letters = "abc defghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPQRSTUVWXYZ0123456789"
         return String((0..<length).map{ _ in letters.randomElement()! })
     }
     
